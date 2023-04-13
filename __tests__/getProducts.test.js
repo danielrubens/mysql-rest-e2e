51c5955d4482ba186b494186ad26d41c215528f9
daniel <danielrubensdaniel@gmail.com>
2023-04-13 00:02:47 -0300

    test: getRoute function

--- __tests__/getProducts.test.js
+++ __tests__/getProducts.test.js
@@ -10,28 +10,32 @@ describe("01 - Create endpoint to list all products", () => {
   beforeAll(async () => await runSeed());
   afterAll(async () => await connect().end());
 
-  it("Exists route /products", async () => {
-    const { status } = await frisby.get(`${url}/products`);
+  const getRoute = async (endpoint) => {
+    const { status, json } = await frisby.get(`${url}${endpoint}`)
+    return { status, json }
+  }
 
-    expect(status).toBeLessThan(400);
+  it("Exists route /products", async () => {
+    const {status} = await getRoute('/products');
+    expect(status).toEqual(200);
   });
 
   it("List all products", async () => {
-    const { status, json } = await frisby.get(`${url}/products`);
+    const { status, json } = await getRoute('/products');
 
     expect(status).toBe(200);
     expect(json).toEqual(allProducts);
   });
 
   it("Impossible to list an unexistent product", async () => {
-    const { status, json } = await frisby.get(`${url}/products/999`);
+    const { status, json } = await getRoute('/products/999');
 
     expect(status).toBe(404);
     expect(json.message).toEqual("Product not found");
   });
 
   it("Can list a product with an specific id", async () => {
-    const { status, json } = await frisby.get(`${url}/products/1`);
+    const { status, json } = await getRoute('/products/1');
 
     expect(status).toBe(200);
     expect(json).toEqual(allProducts[0]);
